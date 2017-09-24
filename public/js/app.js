7bc16facd491fde2d2ffadde6cf03ff703215607
Owen Essen <abc_abcwebd@yahoo.com>
2017-09-24 13:50:50 -0400

    Client side JS can now capture values from dynamically generated forms on page.

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,53 @@ $( document ).ready(function() {
     $( "#scrape-btn" ).click(function() {
 		$.get("/scrape", function(data) {
 			console.log(data);
+			location.reload();
 		});
 	});
+
+	$( ".comment-submit-btn" ).click(function() {
+		var clickedID = $(this).attr('data-article');
+		console.log(clickedID);
+		var title;
+		var comment;
+
+		// Finding the right info
+		$( ".title-input" ).each(function( index ) {
+	  		if ($(this).attr('data-article') === clickedID){
+	  			title = $(this).val();
+	  			console.log("Title: " + title);
+	  			return;
+	  		};
+		});
+
+		$( ".comment-input" ).each(function( index ) {
+	  		if ($(this).attr('data-article') === clickedID){
+	  			comment = $(this).val();
+	  			console.log("Message: " + comment);
+	  			return;
+	  		};
+		});
+/*
+		$.ajax({
+		    method: "POST",
+		    url: "/articles/" + clickedID,
+		    data: {
+		      // Value taken from title input
+		      title: $("#titleinput").val(),
+		      // Value taken from note textarea
+		      body: $("#bodyinput").val()
+	    }
+	  })
+	    // With that done
+	    .done(function(data) {
+	      // Log the response
+	      console.log(data);
+	      // Empty the notes section
+	      $("#notes").empty();
+	    });
+
+	  // Also, remove the values entered in the input and textarea for note entry
+	  $("#titleinput").val("");
+	  $("#bodyinput").val(""); */
+		});
 });
