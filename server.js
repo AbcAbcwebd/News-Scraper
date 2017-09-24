794f5ba8fac592fe34a0cb56bbccca9d1c07c1dc
Owen Essen <abc_abcwebd@yahoo.com>
2017-09-24 10:37:18 -0400

    Initial project setup.

---
+++ server.js
@@ -0,0 +1,7 @@
+// Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const expressHandlebars = require("express-handlebars");
+const cheerio = require("cheerio");
+const request = require("request");
